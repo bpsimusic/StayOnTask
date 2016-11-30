51777894d3feb05dd7af9bc9b5993f5e58204002
bpsimusic <bpsimusic@gmail.com>
2016-11-30 12:11:39 -0800

    stuck on removing unique button

--- sidenav.js
+++ sidenav.js
@@ -1,10 +1,41 @@
 
 document.addEventListener("DOMContentLoaded", function(){
   let hamburger = document.getElementById("hamburger");
-  let mySideNav = document.getElementById("mySideNav");
+  let closeButton = document.getElementById("closebtn");
   hamburger.addEventListener("click", openNav);
-  mySideNav.addEventListener("click", closeNav);
+  closeButton.addEventListener("click", closeNav);
+
+  let form = document.getElementById("website-form");
+  form.addEventListener('submit', function(){
+    let website = document.getElementById("website-entry").value;
+    let key = generateKey();
+    chrome.storage.sync.set({[key]: website});
+  });
+
+
+  let blockedWebsites = document.getElementById("blocked-websites");
+  chrome.storage.sync.get(null, function(items){
+
+    for(var key in items) {
+      let item = document.createElement("li");
+      let remove = document.createElement("button");
+      remove.innerHTML = "remove";
+      let buttonKey = remove.createAttribute = ("key");
+      buttonKey.value = `${key}`;
+      debugger
+      remove.addEventListener("click", function(e){
+        e.preventDefault();
+        debugger
+        chrome.storage.sync.remove(this.getAttribute(key));
+        this.parentElement.remove();
+      });
+      item.appendChild(document.createTextNode(items[key]));
+      item.appendChild(remove);
+      blockedWebsites.appendChild(item);
+    }
+  });
 })
+
 
 
 function openNav() {
@@ -13,4 +44,9 @@ function openNav() {
 
 function closeNav() {
     document.getElementById("mySideNav").style.width = "0";
+}
+
+//submit urls section
+function generateKey() {
+    return ("0000" + (Math.random()*Math.pow(36,4) << 0).toString(36)).slice(-4)
 }
