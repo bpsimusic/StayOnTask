d0d64a4bc627c6bd79fac612c9977a8ea6a6f293
bpsimusic <bpsimusic@gmail.com>
2016-11-30 18:56:31 -0800

    add and delete websites freely

--- sidenav.js
+++ sidenav.js
@@ -1,33 +1,35 @@
 
+
+
+
+
 document.addEventListener("DOMContentLoaded", function(){
+
   let hamburger = document.getElementById("hamburger");
   let closeButton = document.getElementById("closebtn");
   hamburger.addEventListener("click", openNav);
   closeButton.addEventListener("click", closeNav);
 
   let form = document.getElementById("website-form");
-  form.addEventListener('submit', function(){
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
     let website = document.getElementById("website-entry").value;
     let key = generateKey();
     chrome.storage.sync.set({[key]: website});
+    form.reset();
   });
-
-
+  //creating the api
   let blockedWebsites = document.getElementById("blocked-websites");
   chrome.storage.sync.get(null, function(items){
-
-    for(var key in items) {
+    for(let key in items) {
       let item = document.createElement("li");
       let remove = document.createElement("button");
       remove.innerHTML = "remove";
-      let buttonKey = remove.createAttribute = ("key");
-      buttonKey.value = `${key}`;
-      debugger
+      remove.setAttribute("key", key);
       remove.addEventListener("click", function(e){
-        e.preventDefault();
-        debugger
-        chrome.storage.sync.remove(this.getAttribute(key));
-        this.parentElement.remove();
+        chrome.storage.sync.remove(this.getAttribute("key"));
+        let that = this.parentElement
+        blockedWebsites.removeChild(that);
       });
       item.appendChild(document.createTextNode(items[key]));
       item.appendChild(remove);
@@ -35,8 +37,6 @@ document.addEventListener("DOMContentLoaded", function(){
     }
   });
 })
-
-
 
 function openNav() {
     document.getElementById("mySideNav").style.width = "250px";
@@ -50,3 +50,24 @@ function closeNav() {
 function generateKey() {
     return ("0000" + (Math.random()*Math.pow(36,4) << 0).toString(36)).slice(-4)
 }
+
+//adds a new website on the fly
+chrome.storage.onChanged.addListener(function(changes, namespace){
+  for(let key in changes){
+    if (changes[key]["newValue"] === undefined){
+      break;
+    }
+    let blockedWebsites = document.getElementById("blocked-websites");
+    let item = document.createElement("li");
+    let remove = document.createElement("button");
+    remove.innerHTML = "remove";
+    remove.setAttribute("key", key);
+    remove.addEventListener("click", function(e){
+      chrome.storage.sync.remove(this.getAttribute("key"));
+      blockedWebsites.removeChild(this.parentElement);
+    });
+    item.appendChild(document.createTextNode(changes[key]["newValue"]));
+    item.appendChild(remove);
+    blockedWebsites.appendChild(item);
+  }
+})
